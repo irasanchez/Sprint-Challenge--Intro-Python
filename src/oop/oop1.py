e754f696d7e974e0baabe85b5bd430bfd725d3ce
Ira <ira.d.sanchez@gmail.com>
2020-01-24 19:27:17 -0700

    Add Completed oop1.py
    - Add classes and comments that describe them
    - Class bodies only contain "pass"

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,45 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base class Vehicle
+
+
+class Vehicle():
+    pass
+
+
+# GroundVehicle "is a" Vehicle relationship
+
+class GroundVehicle(Vehicle):
+    pass
+
+# FlightVehicle "is a" Vehicle relationship
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+# Starship "is a" FlightVehicle relationship
+
+class Starship(FlightVehicle):
+    pass
+
+
+# Airplane "is a" FlightVehicle relationship
+
+class Airplane(FlightVehicle):
+    pass
+
+
+# Car "is a" GroundVehicle relationship
+
+class Car(GroundVehicle):
+    pass
+
+
+# Motorcycle "is a" GroundVehicle relationship
+
+class Motorcycle(GroundVehicle):
+    pass
